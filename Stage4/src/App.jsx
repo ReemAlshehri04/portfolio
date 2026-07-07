9d33b68104acbfbf27b262bd2d2c263b228465e8
your0T0 <yara241020@gmail.com>
2026-07-07 15:41:56 +0300

    adding restaurants page and admin dashboard page

--- Stage4/src/App.jsx
+++ Stage4/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
+import Restaurants from "./pages/Restaurants/Restaurants";
+import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/restaurants" element={<Restaurants />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
