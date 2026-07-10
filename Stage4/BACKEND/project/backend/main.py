98c2ee8b450fe1011315e637f3748b9459eeefe5
Modi-01 <moudhi.a.almutlaq@gmail.com>
2026-07-11 00:08:54 +0300

    Add meal and subscription routes to main.py

--- Stage4/BACKEND/project/backend/main.py
+++ Stage4/BACKEND/project/backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import get_db_connection
 from routes.auth_routes import router as auth_router
+from routes.meal_routes import router as meal_router
+from routes.subscription_routes import router as subscription_router
 from routes.admin_routes import router as admin_router
 from routes.restaurant_routes import router as restaurant_router
 
@@ -18,3 +20,13 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(admin_router)
 app.include_router(restaurant_router)
+app.include_router(meal_router)
+app.include_router(subscription_router)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Qooti Backend API is running.",
+        "version": "1.0.0"
+    }
